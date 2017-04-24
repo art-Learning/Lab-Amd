88f0bfe03e58ed770e5d6567855a49060007cdcb
art <art@cowell.com.tw>
2017-04-24 17:00:59 +0800

    Use Require Js

---
+++ app/example/modules/displayUtil.js
@@ -0,0 +1,22 @@
+define([
+    'counter'
+], function(counterModule) {
+    function printCount(){
+        var count = counterModule.getCount();
+        if(count===1){
+            count = count + " time";
+        }else{
+            count = count + " times";
+        }
+        console.log("count incremented: "+ count);
+    }
+    function displayNewCount(){
+        counterModule.incrementCount();
+        this.printCount();
+    }
+    return {
+        printCount:printCount,
+        displayNewCount:displayNewCount
+    }
+    
+});
